6eb527dd0d8b521bac46eb8f6fe1030a58d7b731
GayathriPrabha <gayathri6996@gmail.com>
2016-10-07 14:34:34 +0530

    Create days.c

---
+++ days.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("1.Sunday 2. Monday  3.Tuesday  4. Wednesday   5.Thursday  6.Friday  7.Saturday\n");
+scanf("%d",&n);
+switch(n)
+{
+case 1: printf("False");
+break;
+case 2: printf("True");
+break;
+case 3: printf("True");
+break;
+case 4: printf("True");
+break;
+case 5: printf("True");
+break;
+case 6: printf("True");
+break;
+case 7: printf("False");
+break;
+}
+}
